096ce86f4cbe34597c589066446eb1934b7ebebd
Verneckjuliana <verneckjuliana1@gmail.com>
2024-04-08 16:25:54 -0300

    exercicios

---
+++ exercicios-conhecendo-a-base/src/DiasDeIdade.java
@@ -0,0 +1,14 @@
+/*
+ * Exercício 2
+ */
+
+import java.util.Scanner;
+
+public class DiasDeIdade {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
